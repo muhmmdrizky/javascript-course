3d5a8ffd106673e871023bbfd4122c677203f6f6
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-03-07 06:43:37 +0700

    javascript course

--- 05-Functional-Programming/03-Reusable-Function.js
+++ 05-Functional-Programming/03-Reusable-Function.js
@@ -35,16 +35,3 @@ const filteredStudents = students.filter(function (std) {
   return std.class == 10 ? true : false;
 });
 console.log(filteredStudents);
-
-// Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
-function fiveAndGreaterOnly(arr) {
-  return arr.filter((number) => number >= 5);
-}
-// test
-console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
-
-// Given an array of numbers, return a new array that only includes the even numbers.
-function evensOnly(arr) {
-  return arr.filter((number) => number % 2 === 0);
-}
-console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
